e5f8011e8d4d72329f8dc1ca5a01ce505fa26118
GuoChuanrui <guochuanrui@sina.cn>
2016-09-17 22:37:22 +0800

    2016-9-17

--- html_parser.py
+++ html_parser.py
@@ -10,12 +10,13 @@ class HtmlParser(object):
 		links = soup.find_all('a',href = re.compile(r"/view/\d+\.htm"))
 		for link in links:
 			new_url = link['href']
-			new_full_url = urlparse.urljoin(page_url,,new_url)
+			new_full_url = urlparse.urljoin(page_url,new_url)
 			new_urls.add(new_full_url)
 		return new_urls
 
 	def _get_new_data(self,page_url,soup):
 		res_data = {}
+		res_data['url'] = page_url
 		title_node = soup.find('dd',class_ = "lemmaWgt-lemmaTitle-title").find("h1")
 		res_data['title'] = title_node.get_text()
 
